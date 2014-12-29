3289c17836ce9a4c987880e27e0bfe7a9c3556ba
satz <sathish.thi@gmail.com>
2014-12-30 00:13:52 +0530

    working businessinfo CURD operation

--- app/menutang/Repositories/ManageBusinessRepository/ManageBusinessRepository.php
+++ app/menutang/Repositories/ManageBusinessRepository/ManageBusinessRepository.php
@@ -106,4 +106,48 @@ class ManageBusinessRepository extends BaseRepository implements IManageBusiness
     }
 
 
+    /**
+     * @param array $input
+     * @return mixed
+     */
+    public function insert(array $input)
+    {
+        $businessInfo = new $this->model;
+        $businessInfo->business_name = $input['business_name'];
+        $businessInfo->budget = $input['budget'];
+        $businessInfo->is_outdoor_catering = $input['is_outdoor_catering'];
+        $businessInfo->outdoor_catering_comments = $input['outdoor_catering_comments'];
+        $businessInfo->is_door_delivery = $input['is_door_delivery'];
+        $businessInfo->minimum_delivery_amt = $input['is_door_delivery'] ? $input['minimum_delivery_amt'] : 0.00;
+        $businessInfo->is_rail_delivery = $input['is_rail_delivery'];
+        $businessInfo->minimum_rail_deli_amt = $input['is_rail_delivery'] ? $input['minimum_rail_deli_amt'] : 0.00;
+        $businessInfo->is_pickup_available = $input['is_pickup_available'];
+        $businessInfo->minimum_pickup_amt = $input['is_pickup_available'] ? $input['minimum_pickup_amt'] : 0.00;
+        $businessInfo->is_party_hall = $input['is_party_hall'];
+        $businessInfo->party_hall_comments = $input['party_hall_comments'];
+        $businessInfo->is_buffet = $input['is_buffet'];
+        $businessInfo->is_midnight_buffet = $input['is_midnight_buffet'];
+        $businessInfo->is_wifi_available = $input['is_wifi_available'];
+        $businessInfo->is_children_play_area = $input['is_children_play_area'];
+        $businessInfo->is_garden_restaurant = $input['is_garden_restaurant'];
+        $businessInfo->is_roof_top = $input['is_roof_top'];
+        $businessInfo->is_valet_parking = $input['is_valet_parking'];
+        $businessInfo->is_boarding = $input['is_boarding'];
+        $businessInfo->boarding_comments = $input['boarding_comments'];
+        $businessInfo->is_bar_attached = $input['is_bar_attached'];
+        $businessInfo->is_highway_res = $input['is_highway_res'];
+        $businessInfo->highway_details = $input['highway_details'];
+        $businessInfo->website = $input['website'];
+        $businessInfo->avg_delivery_time = $input['avg_delivery_time'];
+        $businessInfo->ischeckout_enable = $input['avg_delivery_time'];
+        $businessInfo->status_id = $input['status_id'];
+        $businessInfo->save();
+        $slug = $this->slug($input['business_name']);
+        if (!empty($this->findBusinessBySlug($slug))) {
+            $slug = $slug . '_' . $businessInfo->id;
+        }
+        $businessInfo->fill(['business_slug' => $slug]);
+        $businessInfo->save();
+
+    }
 }
