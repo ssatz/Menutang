f386f938498af1a88870a7e15cf9cda73c8a2694
Satz <sathish.thi@gmail.com>
2015-03-11 22:11:35 +0530

    working on About us page

---
+++ app/views/frontend/about.blade.php
@@ -0,0 +1,19 @@
+@extends('frontend.index')
+
+@section('content')
+
+    <header class="home" style="height: 55px">
+     </header>
+    <section class="bg-light">
+    <div class="container-fluid">
+        <div class="row">
+            <h1>About Us</h1>
+           <p class="pad10">Menutang is Online Food Ordering/Search Portal, which enables you to search or Order the best food industries in your town .</p>
+            Menutang has information on Restaurant menus and their other informations , which enables to plan your occassions .
+            You can place an order with us so that ensure  that you enjoy your food on the estimated time .
+            Mentang also provide you reviews so that you can identify the best informations with us .
+
+        </div>
+    </div>
+    </section>
+@overwrite
