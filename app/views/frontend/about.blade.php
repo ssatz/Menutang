9d2ef5df4130e42c00d800fe1e538dc55d503366
Satz <sathish.thi@gmail.com>
2015-03-12 11:14:32 +0530

    working on About us page

--- app/views/frontend/about.blade.php
+++ app/views/frontend/about.blade.php
@@ -3,17 +3,51 @@
 @section('content')
 
     <header class="home" style="height: 55px">
-     </header>
+    </header>
     <section class="bg-light">
-    <div class="container-fluid">
-        <div class="row">
-            <h1>About Us</h1>
-           <p class="pad10">Menutang is Online Food Ordering/Search Portal, which enables you to search or Order the best food industries in your town .</p>
-            Menutang has information on Restaurant menus and their other informations , which enables to plan your occassions .
-            You can place an order with us so that ensure  that you enjoy your food on the estimated time .
-            Mentang also provide you reviews so that you can identify the best informations with us .
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12">
+                        <article>
+                            <h3 class="title">About Us</h3>
 
+                            <div class="post-content">
+                                <p>
+                                    Menutang is Online Food Ordering/Search Portal, which enables you to search or Order
+                                    the best food industries in your town.
+                                    Menutang has information on Restaurant menus and their other informations , which
+                                    enables to plan your occassions .
+                                    You can place an order with us so that ensure that you enjoy your food on the
+                                    estimated time .
+                                    Menutang also provide you reviews so that you can identify the best informations
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                        <div class="contact-box widget">
+                            <h3>More Questions?</h3>
+                            <i class="fa fa-envelope"></i>
+
+                            <p>Let us know your question via the contact form.</p>
+                        </div>
+                    </div>
+                    <!--<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                        <div class="contact-box widget">
+                            <h3>Business Hour</h3>
+                            <i class="fa fa-clock-o"> </i>
+                            <ul>
+                                <li>Monday - Friday 9am to 5pm</li>
+                                <li>Saturday - 9am to 2pm</li>
+                                <li>Sunday - Closed</li>
+                            </ul>
+                        </div>
+                    </div>-->
+
+                </div>
+            </div>
         </div>
-    </div>
+
     </section>
 @overwrite
