173a339c162e63a3f5a73b3804afd9b5ca91f6a8
satz <sathish.thi@gmail.com>
2015-03-15 09:50:45 +0530

    working on Password Reset, Also added Events for Mail

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,9 +24,9 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-    'local' => ['*.dev', gethostname()],
-));
+$env = $app->detectEnvironment([
+    'local' => ['Admin-PC'],
+]);
 
 /*
 |--------------------------------------------------------------------------
