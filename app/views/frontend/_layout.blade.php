16a36bb2573b2ca0b5f992ec991e8d262d6d6a38
satz <sathish.thi@gmail.com>
2015-05-22 17:40:08 +0530

    removed search bar in the top

--- app/views/frontend/_layout.blade.php
+++ app/views/frontend/_layout.blade.php
@@ -50,13 +50,13 @@
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <form class="navbar-form navbar-left" role="search">
+               <!-- <form class="navbar-form navbar-left" role="search">
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="City Name" value="Mumbai">
                     </div>
                     <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i> Search
                     </button>
-                </form>
+                </form>-->
                 <ul class="nav navbar-nav navbar-right borderStyle">
                     @if(Auth::user()->check())
                     <li class="dropdown dropdown-menutang"><a href="#" id="drop1" data-toggle="dropdown"
