758f8cc4e517004adf7aa224d72d7b3d2f9c781b
satz <sathish.thi@gmail.com>
2015-07-15 13:24:36 +0530

    WIP: user profile

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -22,10 +22,10 @@ return array(
 
         'mysql' => array(
             'driver' => 'mysql',
-            'host' => getenv('DB_HOST'),
-            'database' => getenv('DB_NAME'),
-            'username' => getenv('DB_USER'),
-            'password' => getenv('DB_PASS'),
+            'host' => 'localhost',
+            'database' => 'menutang',
+            'username' => 'forge',
+            'password' => 'GrWgR7eWkPiONKqCNtJI',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
